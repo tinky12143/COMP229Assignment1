3a58a7a5c2d9b45aca33ec2a979d61c112673587
tinky12143 <tinky12143@outlook.com>
2023-06-10 00:43:23 -0400

    Fix 404 error on Heroku

--- app.js
+++ app.js
@@ -22,11 +22,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
 
 
 // Use routers
-// app.use('/', indexRouter); // currently commented out
+ app.use('/', indexRouter); 
 app.use('/users', usersRouter);
 
 // Catch 404 and forward to error handler
@@ -46,9 +45,9 @@ app.use(function(err, req, res, next) {
 });
 
 // Define route for the homepage
-app.get('/', function(req, res) {
-  res.render('index');
-});
+//app.get('/', function(req, res) {
+//  res.render('index');
+//});
 
 // Export the app so it can be used in other modules
 module.exports = app;
